42a63d1a9c573493b2bbecefa7f577a76115d572
Tirth-Hansaliya <147802041+Tirth-Hansaliya@users.noreply.github.com>
2025-07-07 13:00:03 +0530

    Delete Finance_Flow_project/Finance_Flow_app directory

--- Finance_Flow_project/Finance_Flow_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-class Reg_(admin.ModelAdmin):
-    list_display = ['id','name','email','mob','add']
-
-admin.site.register(Registration,Reg_)
--- Finance_Flow_project/Finance_Flow_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FinanceFlowAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Finance_Flow_app'
