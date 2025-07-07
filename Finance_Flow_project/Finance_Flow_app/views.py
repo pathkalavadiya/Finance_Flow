88a08d3172deab69d89be0351e63d435b2e8501a
Tirth-Hansaliya <tirthhansaliya1515@gmail.com>
2025-07-07 12:21:35 +0530

    deshbord

--- Finance_Flow_project/Finance_Flow_app/views.py
+++ Finance_Flow_project/Finance_Flow_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Registration
+
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        store_reg = Registration()
+        store_reg.name = request.POST['name']
+        store_reg.email = request.POST['email']
+        store_reg.add = request.POST['add']
+        store_reg.mob = request.POST['mob']
+        store_reg.password = request.POST['password']
+        try:
+            already_registered = Registration.objects.get(email = request.POST['email'])
+            if already_registered:
+                return render(request,'register.html',{'already_registerd':"This email is already registered.."})
+        except:
+            store_reg.save()
+            return render(request,'register.html',{'stored':"registration sucessfull.."})
+    else:
+        return render(request,'register.html')
